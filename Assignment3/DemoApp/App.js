b56b50e93dd6a804509dd3574b895bfa1ae5b65e
AliBinQamar <Aliq2726686@gmail.com>
2025-11-25 19:20:16 +0500

    Background Color Changed

--- Assignment3/DemoApp/App.js
+++ Assignment3/DemoApp/App.js
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#e0f7fa', // soft modern background
+    backgroundColor: '#ddfbffff', // soft modern background
   },
   title: {
     fontSize: 32,
